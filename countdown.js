d31739e4bf5f8aac6a440eb880f0d5e079cf3a36
z-sohaib <js_zouambia@esi.dz>
2021-09-10 15:48:13 +0100

    added zero

--- countdown.js
+++ countdown.js
@@ -20,6 +20,7 @@ function getTimeRemaining(endtime) {
   
       minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
       secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+      console.log(t.total);
       if (t.total <= 2592000000) {
         clearInterval(timeinterval);
       }
@@ -29,5 +30,5 @@ function getTimeRemaining(endtime) {
     const timeinterval = setInterval(updateClock, 1000);
   }
   
-  const deadline = new Date(Date.parse(new Date(2021, 9, 10, 15, 46, 0, 0)));
+  const deadline = new Date(Date.parse(new Date(2021, 9, 10, 16, 10, 0, 0)));
   initializeClock('clockdiv', deadline);
